95fb896c81ff49ab0e1abe7bca887d39c2d77ad2
Sylvain Bellemare <sbellem@gmail.com>
2015-11-29 16:56:58 +0100

    increase minor version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ascribe',
-    version='0.0.6.dev1',
+    version='0.0.7.dev1',
     description='ascribe api',
     long_description=long_description,
     url='https://ascribe.readthedocs.org',
